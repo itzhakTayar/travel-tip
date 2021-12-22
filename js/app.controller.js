56417445588b1416e9f49d82701d152db45dd511
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2021-12-22 15:45:04 +0200

    first commit

--- js/app.controller.js
+++ js/app.controller.js
@@ -23,10 +23,10 @@ function getPosition() {
     })
 }
 
-function onAddMarker() {
-    console.log('Adding a marker');
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
-}
+// function onAddMarker() {
+//     console.log('Adding a marker');
+//     mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
+// }
 
 function onGetLocs() {
     locService.getLocs()
